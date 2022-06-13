055952616a613946bf95a1ad7946ddd3ca616e9a
Mandar Bedekar <mandar.bedekar2007@gmail.com>
2022-06-13 15:31:40 -0700

    Update streamlit_app.py
    Added new header

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,10 @@ import streamlit
 streamlit.title('New Healthy Diner')
 
 streamlit.header('Breakfast Favorites')
+
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔 Hard Boiled Free-range Eggs')
 streamlit.text('🥑🍞 Avocado Toast')
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
