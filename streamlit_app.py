142680e2b553ce9aeb70525180414d719e59572c
Mandar Bedekar <mandar.bedekar2007@gmail.com>
2022-06-13 15:23:42 -0700

    Update streamlit_app.py
    added menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,8 @@
 
 import streamlit
 streamlit.title('New Healthy Diner')
+
+streamlit.header('Breakfast Menu')
+streamlit.test('Omega 3 & Blueberry Oatmeal')
+streamlit.test('Kale, Spinach & Rocket Smoothie')
+streamlit.test('Hard Boiled Free-range Eggs')
